8307050cd06729b42708131260556a90de256bfe
Eugun <59265316+EugunHribinchak@users.noreply.github.com>
2023-01-28 13:22:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,39 +1,59 @@
 ﻿using System;
-//Перевірити істинність вислову: "Дані числа x, у є координатами точки, що лежить у другому квадранті ".
-//Перевірити істинність вислову: "Дані числа x, у є координатами точки, що лежить у першому або третьому квадранті". 
+//Перевірити істинність вислову: "Точка з координатами (x, у) лежить всередині прямокутника, ліва верхня вершина якого має координати (x1, y1), права нижня — (x2, y2),
+//а сторони паралельні координатним осям".
 
-namespace Ex_2
+namespace Ex_4
 {
     class Program
     {
         static void Main(string[] args)
         {
             string str;
-            int x,y;
+            int x1, y1,x2,y2,x,y;
 
-            Console.WriteLine("Введите число x");
+            Console.WriteLine("Write X");
             str = Console.ReadLine();
             x = Convert.ToInt32(str);
-            Console.WriteLine("Введите число y");
+            Console.WriteLine("Write Y");
             str = Console.ReadLine();
             y = Convert.ToInt32(str);
-            if (x<0 && y>0)
+
+            do
             {
-                Console.WriteLine("Лежить у першому квадрати");
-            }
-            else if (x > 0 && y > 0)
+                Console.WriteLine("Write X1");
+                str = Console.ReadLine();
+                x1 = Convert.ToInt32(str);
+            } while (x1 > 0);
+            do
             {
-                Console.WriteLine("Лежить у другому квадрати");
-            }
-            else if (x > 0 && y < 0)
+                Console.WriteLine("Write Y1");
+                str = Console.ReadLine();
+                y1 = Convert.ToInt32(str);
+            } while (y1 < 0);
+            Console.WriteLine("Левая верхняя точка имеет координаты"+x1+' '+y1);
+
+            do
             {
-                Console.WriteLine("Лежить у третьому квадрати");
-            }
-            else if (x < 0 && y < 0)
+                Console.WriteLine("Write X2");
+                str = Console.ReadLine();
+                x2 = Convert.ToInt32(str);
+            } while (x2 < 0);
+            do
             {
-                Console.WriteLine("Лежить у четвертому квадрати");
+                Console.WriteLine("Write Y2");
+                str = Console.ReadLine();
+                y2 = Convert.ToInt32(str);
+            } while (y2 > 0);
+            Console.WriteLine("Правая нижняя точка имеет координаты" + x2 + ' ' + y2);
+
+            if (x>x1 && x<x2 && y <y1 && y>y2)
+            {
+                Console.WriteLine("Точка лежит в квадрате");
             }
-            else { Console.WriteLine("На початку коордынат"); }
+            else
+            {
+                Console.WriteLine("Точка не лежит в квадрате");
+            }
         }
     }
 }
